84b26e38b0f572d56e3b6e4b4c6fc3fcdca9672d
niaw junior <niawkung2@gmail.com>
2017-05-28 22:55:45 +0700

    Brick Field Collision  Detection

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var bricks = [];
 for(c=0; c<brickColumnCount; c++){
     bricks[c] = [];
     for(r=0; r< brickRowCount; r++){
-        bricks[c][r] = {x: 0, y:0}
+        bricks[c][r] = {x: 0, y:0,status:1}
     }
 }
 
@@ -33,6 +33,7 @@ document.addEventListener("keyup",keyUpHandler);
 function drawBricks() {
 	for(c=0; c<brickColumnCount; c++) {
 		for(r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status ==1){
 				var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
 				var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
 				bricks[c][r].x = brickX;
@@ -42,6 +43,7 @@ function drawBricks() {
 				ctx.fillStyle = "#0095DD";
 				ctx.fill();
 				ctx.closePath();
+            }
 		}
 	}
 }
@@ -79,12 +81,26 @@ function drawPaddle() {
 	ctx.closePath();
 }
 
+function collisionDetection(){
+    for(c=0;c<brickColumnCount;c++){
+        for(r=0;r<brickRowCount;r++){
+            var b = bricks[c][r];
+            if(b.status == 1){
+                if(x > b.x && x < b.x+brickWidth && y > b.y &&  y  < b.y+brickHeight){
+                    dy = -dy;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
 
 function draw() {
     ctx.clearRect(0,0, canvas.width, canvas.height);
     drawBricks()
     drawBall();
 	drawPaddle();
+    collisionDetection();
 
     if(y + dy < ballRadius ){
         dy = -dy;
